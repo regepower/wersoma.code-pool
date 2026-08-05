55888415c9a4b0398434783e65749e4a9c8d73e5
Rene Trolldenier - Wersoma <Rene.Trolldenier@wersoma.de>
2026-08-05 06:00:13 +0200

    Auto-Update am 05.08.2026 um  6:00:13,25

--- .gitupdate.bat
+++ .gitupdate.bat
@@ -1,26 +1,38 @@
 @echo off
 cd /d "C:\Users\trolldenier\Documents\Makrosammlung.git"
 
-echo Pruefe auf Aenderungen...
+echo ==========================================
+echo  Hole zuerst Updates von GitHub...
+echo ==========================================
+git pull
+
+echo.
+echo ==========================================
+echo  Pruefe nun auf lokale Aenderungen...
+echo ==========================================
 git add .
 
 REM Prüft, ob durch 'git add' wirklich etwas im Staging-Bereich gelandet ist
 git diff --staged --quiet
 if %errorlevel% == 0 (
-    echo Keine neuen Aenderungen gefunden. Alles ist aktuell.
-    timeout /t 5
+    echo Keine neuen Aenderungen gefunden. Alles ist aktuell!
+    timeout /t 3
     exit /b
 )
 
+echo.
 echo Es gibt Aenderungen!
 set /p msg="Bitte kurze Beschreibung eingeben (oder Enter fuer Auto-Text): "
 
 if "%msg%"=="" set msg=Auto-Update am %date% um %time%
 
+echo.
 echo Erstelle Commit und lade hoch...
 git commit -m "%msg%"
 git push
 
 echo.
-echo Erfolgreich auf GitHub hochgeladen!
-timeout /t 50
+echo ==========================================
+echo  Erfolgreich auf GitHub hochgeladen!
+echo ==========================================
+timeout /t 5
